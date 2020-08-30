a7fbe07785169d7ad37bcd5355d8243630f7365e
Jason Upchurch <jmu7av@virginia.edu>
2020-08-30 14:47:59 -0400

    Update projects

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -9,7 +9,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="Blog" />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.frontmatter.slug
         return (
