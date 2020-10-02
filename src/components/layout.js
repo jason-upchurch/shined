d4b1ce4bf0a354f21f47822a3a5407b08aabc526
Jason Upchurch <jmu7av@virginia.edu>
2020-10-02 08:39:49 -0400

    Update visibility sensor for title scroll

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
       <div style={{
 	height: `auto`,
 	width: `auto`,
-	maxWidth: `1900px`,
+	maxWidth: `undefined`,
 	position: `relative`,
 	margin: `auto`,
 	top: `0vh`,
@@ -40,40 +40,44 @@ const Layout = ({ children }) => {
       position: `absolute`,
       width: `100%`,
       minWidth: `79.64vh`,
-      top: `35vh`,
+      top: `25vh`,
       color: `black`,
       borderColor: `black`,
-    }}
-    
-
-      >
-      <div style={{
-	width: `auto`,
-	height: '50vh',
-	backgroundColor: `green`,
-	opacity: `0.2`,
-      }} />
-      </Parallax>
-
-    </div>
-            	  <VisibilitySensor once>
+    }}>
+	        <VisibilitySensor once>
 	  {({ isVisible }) => (
-	      <Spring delay={500} to={{ opacity: isVisible ? 1 : 0 }}>
+	      <Spring delay={25} to={{ opacity: isVisible ? 1 : 0 }}>
 	      {({ opacity }) =>
 	       <h1 style={{
 		 opacity,
 		 fontSize: `75px`,
-		 textAlign: `center`,
-		 top: `0px`,
-		 position: `relative`,
+		   textAlign: `center`,
+		   height: `75px`,
+		 paddingTop: `25vh`,
+		   position: `relative`,
+		   color: `goldenrod`,
 	       }}>shined.org</h1>
 	      }
 	    </Spring>
 	  )}
 	</VisibilitySensor>
+      <div style={{
+	width: `auto`,
+	  height: '25vh',
+	  top: `0vh`,
+      }} />
+
+
+      </Parallax>
+  
+    </div>
 
       <main style={{
-	backgroundColor: `yellow`}}>{children}</main>
+	  maxWidth: `960px`,
+	  margin: `0 auto`,
+      }}>
+	  {children}
+      </main>
       <footer style={{
           margin: `0 auto`,
 	backgroundColor: `orange`,
