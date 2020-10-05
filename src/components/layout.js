c495c02131646b9e346627611062e316e203c9dc
Jason Upchurch <jmu7av@virginia.edu>
2020-10-04 21:28:51 -0400

    Increase hero size

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Hero from "./Hero"
-import hero from "../images/potw2033a.jpg"
+import hero from "../images/herosm.png"
 import Navbar from "./Navbar/Navbar"
 import "./layout.css"
 import { Parallax, Background } from "react-parallax"
@@ -19,8 +19,8 @@ import { Spring } from "react-spring/renderprops"
 const Layout = ({ children }) => {
   return (
 	  <div style={{
-	      backgroundColor: `gray`,
-	      	paddingTop: `1rem`,
+	    backgroundColor: `gray`,
+	paddingTop: `1rem`,
 	paddingBottom: `1rem`,
 	paddingLeft: `1rem`,
 	paddingRight: `1rem`,
@@ -30,10 +30,9 @@ const Layout = ({ children }) => {
 
 
       <div style={{
-	height: `95%`,
-	width: `95%`,
-	  maxWidth: `undefined`,
-	  	paddingTop: `1rem`,
+	height: `100vh`,
+	width: `100%`,
+	 paddingTop: `1rem`,
 	paddingBottom: `1rem`,
 	paddingLeft: `1rem`,
 	paddingRight: `1rem`,
@@ -49,9 +48,9 @@ const Layout = ({ children }) => {
     bgImageAlt="the dog"
     strength={900}
     bgImageStyle={{
-	height: `auto`,
-	width: `95%`,
-	top: `400px`,
+      height: `95vh`,
+      minWidth: `145vh`,
+	top: `475px`,
 	position: `absolute`,
 	backgroundColor: `black`,
 	paddingTop: `1rem`,
@@ -64,14 +63,15 @@ const Layout = ({ children }) => {
     }}>
 
 	  <div style={{
-	      height: `50vh`,
+	      height: `90vh`,
 	      width: `auto`,
 	paddingTop: `1rem`,
 	paddingBottom: `1rem`,
 	paddingLeft: `1rem`,
 	paddingRight: `1rem`,
 	  backgroundColor: `blue`,
-	  opacity: `0.5`,
+	    opacity: `0.5`,
+	    margin: `0 auto`,
 	  top: `10vh`,
       }} />
 
@@ -94,7 +94,7 @@ const Layout = ({ children }) => {
       </main>
       <footer style={{
           margin: `0 auto`,
-	backgroundColor: `orange`,
+	backgroundColor: `goldendrod`,
 	  paddingBottom: `1.45rem`,
 	  opacity: `1`,
 	borderColor: `blue`,
