ae762ed6ae6d32b1d746ffbd15b53982470e1fe7
Jason Upchurch <jmu7av@virginia.edu>
2020-10-06 14:52:24 -0400

    Update project and blog with new style

--- src/components/layout-projects.js
+++ src/components/layout-projects.js
@@ -8,44 +8,94 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import Hero from "./Hero-blog"
+import Hero from "./Hero"
+import hero from "../images/PIA23865.jpg"
 import Navbar from "./Navbar/Navbar"
 import "./layout.css"
+import { Parallax, Background } from "react-parallax"
+import VisibilitySensor from "react-visibility-sensor"
+import { Spring } from "react-spring/renderprops"
 
 const Layout = ({ children }) => {
   return (
-      <div
-    style={{
-      maxWidth: `100%`,
-      margin: `0 auto`,
-    }}>
+      <div style={{
+	  backgroundColor: `black`,
+      }}>
       <Navbar/>
-      <Hero />
-      <div
-        style={{
-          margin: `0 auto`,
-	  position: `relative`,
-          maxWidth: `960px`,
-        }}
-      >
-      <div
-    style={{
-      maxWidth: `768px`,
-      position: `relative`,
-      padding: `0px 1.45rem`,
-      margin: `0 auto`,
+
+      <div style={{
+	height: `90vh`,
+	width: `100%`,
+	position: `relative`,
+	backgroundColor: `black`,
+	  margin: `0 auto`,
+	top: `0px`,
+      }}>
+
+
+       <Parallax
+            bgImage={hero}
+    bgImageAlt="the dog"
+    strength={900}
+    bgImageStyle={{
+      height: `90vh`,
+      minWidth: `130.43vh`,
+	top: `485px`,
+	position: `absolute`,
+	margin: `0 auto`,
+
     }}>
-      <main>{children}</main>
-      </div>
-        <footer style={{
-          margin: `0 auto`,
-	  padding: `0px 1.45rem`,
-	  paddingBottom: `1.45rem`
-        }}>
+	  	  	  <VisibilitySensor once>
+	  {({ isVisible }) => (
+	      <Spring delay={50} to={{ opacity: isVisible ? 1 : 0 }}>
+	      {({ opacity }) =>
+	       <h1 style={{
+		   opacity,
+		   height: `75px`,
+		   fontSize: `75px`,
+		   color: `white`,
+		 textAlign: `center`,
+		 paddingTop: `35vh`,
+	       }}>shined.org</h1>
+	        
+	      }
+	    </Spring>
+	  )}
+	</VisibilitySensor>
+	  <div style={{
+	    height: `90vh`,
+	      width: `auto`,
+	    margin: `0 auto`,
+	  top: `10vh`,
+      }} />
+
+      </Parallax>
+  
+    </div>
+
+      <main style={{
+	  width: `960px`,
+	  maxWidth: `90vw`,
+	  position: `relative`,
+	  opacity: `1`,
+	  paddingTop: `0rem`,
+	  paddingBottom: `0rem`,
+	  backgroundColor: `black`,
+	  paddingLeft: `0rem`,
+	  paddingRight: `0rem`,
+	  margin: `0 auto`,
+	  top: `auto`,
+      }}>
+      {children}
+          <footer style={{
+	      color: `white`
+	  }}>
           © {new Date().getFullYear()} Jason Upchurch
         </footer>
+	  </main>
+
+
       </div>
-    </div>
   )
 }
 
