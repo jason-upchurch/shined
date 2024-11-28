62995d48de5a639a85963199a3a026b42a705c6a
Jason Upchurch <jmu7av@virginia.edu>
2024-11-28 08:45:54 -0500

    removed parallax and hero

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,17 +8,18 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import hero from "../images/herosm.png"
+import hero from "../images/hero.png"
 import Navbar from "./Navbar/Navbar"
 import "./layout.css"
-import { Parallax, Background } from "react-parallax"
-import VisibilitySensor from "react-visibility-sensor"
-import { Spring } from "react-spring/renderprops"
 
 const Layout = ({ children }) => {
+
   return (
       <div style={{
-	  backgroundColor: `black`,
+	backgroundColor: `black`,
+	 backgroundImage: `url(${hero})`,
+	backgroundSize: "cover",
+	backgroundPosition: "center"
       }}>
       <Navbar/>
 
@@ -32,66 +33,23 @@ const Layout = ({ children }) => {
       }}>
 
 
-       <Parallax
-            bgImage={hero}
-    bgImageAlt="the dog"
-    strength={900}
-    bgImageStyle={{
-      height: `90vh`,
-      minWidth: `130.43vh`,
-	top: `485px`,
-	position: `absolute`,
-	margin: `0 auto`,
 
-    }}>
-	  	  	  <VisibilitySensor once>
-	  {({ isVisible }) => (
-	      <Spring delay={50} to={{ opacity: isVisible ? 1 : 0 }}>
-	      {({ opacity }) =>
+
 	       <h1 style={{
-		   opacity,
 		   height: `65px`,
 		   fontSize: `65px`,
-		   color: `black`,
+		   color: `white`,
 		 textAlign: `center`,
 		 paddingTop: `35vh`,
-	       }}>shined.org</h1>
+	       }}>jasonupchurch.io</h1>
 	        
-	      }
-	    </Spring>
-	  )}
-	</VisibilitySensor>
-	  <div style={{
-	    height: `90vh`,
-	      width: `auto`,
-	    margin: `0 auto`,
-	  top: `10vh`,
-      }} />
-
-      </Parallax>
   
     </div>
 
-      <main style={{
-	  maxWidth: `960px`,
-	  position: `relative`,
-	  backgroundColor: `white`,
-	  opacity: `1`,
-	  paddingTop: `1rem`,
-	  paddingBottom: `1rem`,
-	  paddingLeft: `1rem`,
-	  paddingRight: `1rem`,
-	  margin: `0 auto`,
-	  top: `auto`,
-      }}>
+
       {children}
-          <footer>
-              © 2020&ndash;{new Date().getFullYear()} Jason Upchurch
-        </footer>
-	  </main>
 
-
-      </div>
+    </div>
   )
 }
 
